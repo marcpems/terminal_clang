6f2600ce01f43060ba88763fb59aaa36f5813cbc
marcpems <marcpe@microsoftcom>
2025-12-22 19:23:29 +0000

    add some more debug msgs

--- perftest/prep.cmd
+++ perftest/prep.cmd
@@ -18,9 +18,16 @@ REM msbuild openconsole.slnx /p:platform=x64;configuration=debug /p:WindowsTermi
 REM echo X64 LLVM DEBUG
 
 REM msbuild openconsole.slnx /p:platform=arm64;configuration=release /t:Conhost\Host_EXE /m 
+REM echo Arm64 MSVC Release
+
 REM msbuild openconsole.slnx /p:platform=arm64;configuration=debug /t:Conhost\Host_EXE /m 
+REM echo Arm64 MSVC Debug
+
 REM msbuild openconsole.slnx /p:platform=arm64;configuration=release /p:WindowsTerminalClangBuild=true /t:Conhost\Host_EXE /m 
+REM echo Arm64 LLVM Release
+
 REM msbuild openconsole.slnx /p:platform=arm64;configuration=debug /p:WindowsTerminalClangBuild=true /t:Conhost\Host_EXE /m 
+REM echo Arm64 LLVM Debug
 
 
 REM msbuild openconsole.slnx /p:platform=x64;configuration=release /t:_Tools\ConsoleBench /m 
