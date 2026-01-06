597ce2db92909643fb18d31049b250c53bc01093
marcpems <marcpe@microsoft.com>
2026-01-06 16:43:26 +0000

    document the setup

--- perftest/runPerfTests.cmd
+++ perftest/runPerfTests.cmd
@@ -39,6 +39,8 @@ if exist "%SUMMARY_FILE%" del "%SUMMARY_FILE%"
 
 REM Loop through all 12 configurations
 REM for %%C in (1 2 3 4 5 6 7 8 9 10 11 12) do (
+
+REM just the LLD configs
 for %%C in (9 10 11 12) do (
     call :RunConfigTests %%C
 )
